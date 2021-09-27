226b672a693aba96d9f442ed73c8d1b8682f4dab
gabriel <puppizin@gmail.com>
2021-09-27 09:49:35 -0300

    Initial Commit

---
+++ growroom/growroomApp.swift
@@ -0,0 +1,17 @@
+//
+//  growroomApp.swift
+//  growroom
+//
+//  Created by Gabriel Puppi on 27/09/21.
+//
+
+import SwiftUI
+
+@main
+struct growroomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
